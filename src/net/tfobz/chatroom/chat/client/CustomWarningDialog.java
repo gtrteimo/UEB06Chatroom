3b8768c98e3a4c948b6a5d9da092c7417ae1bb85
MysticalBaum <danielsupvol@gmail.com>
2025-01-22 09:08:41 +0100

    qwer
    qwer

--- src/net/tfobz/chatroom/chat/client/CustomWarningDialog.java
+++ src/net/tfobz/chatroom/chat/client/CustomWarningDialog.java
@@ -11,37 +11,44 @@ public class CustomWarningDialog extends JDialog {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		getContentPane().setLayout(null);
+		try {
+			setIconImage(new ImageIcon(getClass().getResource("/icons/warningIco.png")).getImage());
+		} catch (NullPointerException e) {
+			System.out.println("Error: Couldnt find Image (warningIco)");
+		}
 
 		JPanel panel = new JPanel();
+		panel.setBounds(0,0,384,127);
+		panel.setBackground(new Color(192, 192, 192));
 		panel.setLayout(null);
 		
 		JLabel iconLabel = new JLabel();
-		iconLabel.setBounds(50,50,60,60);
+		iconLabel.setBounds(10,33,40,50);
 		try {
 			iconLabel.setIcon(new ImageIcon(getClass().getResource("/icons/warningIco.png")));
 		} catch (NullPointerException e) {
 			System.out.println("Error: Couldnt find Image (warningIco)");
 		}
-		
+		iconLabel.setVisible(true);
 		panel.add(iconLabel);
 
 		JLabel messageLabel = new JLabel(message);
-		messageLabel.setFont(new Font("Arial", Font.PLAIN, 26));
+		messageLabel.setFont(new Font("Arial", Font.PLAIN, 28));
 		messageLabel.setForeground(Color.black);
-		messageLabel.setBounds(0,0,50,50);
+		messageLabel.setBounds(60,33,314,50);
 		panel.add(messageLabel);
 
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setBounds(0,126,384,35);
 		JButton okButton = new JButton("OK");
+		okButton.setBackground(buttonPanel.getForeground());
 		okButton.setFont(new Font("Arial", Font.PLAIN, 14));
 		okButton.setForeground(Color.black);
 		okButton.setFocusPainted(false);
 		okButton.setBorderPainted(false);
 		okButton.setBounds(0,50,50,50);
 		okButton.addActionListener(e -> dispose());
-
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setBackground(new Color(192, 192, 192));
-		buttonPanel.setBounds(0,126,384,35);
 		buttonPanel.add(okButton);
 
 		getContentPane().add(panel);
