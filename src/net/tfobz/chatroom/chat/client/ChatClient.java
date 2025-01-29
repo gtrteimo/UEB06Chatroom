85491b80cb9448b140d95c2734bbaa995b63f035
MysticalBaum <danielsupvol@gmail.com>
2025-01-29 09:16:39 +0100

    w
    w

--- src/net/tfobz/chatroom/chat/client/ChatClient.java
+++ src/net/tfobz/chatroom/chat/client/ChatClient.java
@@ -32,21 +32,12 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
-import com.sun.media.jfxmedia.events.NewFrameEvent;
-
 @SuppressWarnings("serial")
-public class ChatClient extends JFrame {	
-	private final static Color colours[][] = {
-        {
-            new Color(50, 50, 75),
-            new Color(171, 181, 216),
-            new Color(225, 225, 225),
-            new Color(11, 9, 10),
-            new Color(111, 116, 146)
-        }
-    };
+public class ChatClient extends JFrame {
+	private final static Color colours[][] = { { new Color(50, 50, 75), new Color(171, 181, 216),
+			new Color(225, 225, 225), new Color(11, 9, 10), new Color(111, 116, 146) } };
 	protected int CLIENT_ID;
-	
+
 	public static final int DEFAULT_PORT = 10000;
 	public static final String DEFAULT_IP = "localhost";
 	public static final String DEFAULT_USERNAME = "User";
@@ -65,7 +56,7 @@ public class ChatClient extends JFrame {
 	private JButton newChatroom;
 	private JButton logIn;
 	private JButton exit;
-	
+
 	private JEditorPane textArea;
 	private JEditorPane textField;
 	private JButton button;
@@ -74,54 +65,62 @@ public class ChatClient extends JFrame {
 
 	private BufferedReader in;
 	private PrintStream out;
-	
+
 	private ExecutorService executer;
-	
-	public ChatClient () {
+
+	public ChatClient() {
 		super();
-				
+
 		executer = Executors.newSingleThreadExecutor();
-		
+
 		setBounds(25, 25, 1080, 720);
 		setResizable(false);
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		setTitle("Main Chatroom");
 
 		addWindowListener(new WindowListener() {
-			
+
 			@Override
 			public void windowClosing(WindowEvent e) {
 				try {
 					logOut();
 					client.close();
 				} catch (NullPointerException | IOException e1) {
-					try {client.close();} catch (Exception e2) {}
+					try {
+						client.close();
+					} catch (Exception e2) {
+					}
 				}
 				setVisible(false);
 				dispose();
 				System.exit(2);
 			}
-			
+
 			@Override
-			public void windowOpened(WindowEvent e) {}
-			
+			public void windowOpened(WindowEvent e) {
+			}
+
 			@Override
-			public void windowIconified(WindowEvent e) {}
-			
+			public void windowIconified(WindowEvent e) {
+			}
+
 			@Override
-			public void windowDeiconified(WindowEvent e) {}
-			
+			public void windowDeiconified(WindowEvent e) {
+			}
+
 			@Override
-			public void windowDeactivated(WindowEvent e) {}
-			
-			
+			public void windowDeactivated(WindowEvent e) {
+			}
+
 			@Override
-			public void windowClosed(WindowEvent e) {}
-			
+			public void windowClosed(WindowEvent e) {
+			}
+
 			@Override
-			public void windowActivated(WindowEvent e) {}
+			public void windowActivated(WindowEvent e) {
+			}
 		});
-		
+
 		contentPane = getContentPane();
 		contentPane.setLayout(null);
 		contentPane.setBackground(colours[0][1]);
@@ -135,7 +134,7 @@ public class ChatClient extends JFrame {
 		newChatroom = new JButton();
 		logIn = new JButton();
 		exit = new JButton();
-		
+
 		textArea = new JEditorPane() {
 			@Override
 			public boolean getScrollableTracksViewportWidth() {
@@ -150,20 +149,21 @@ public class ChatClient extends JFrame {
 		};
 		button = new JButton();
 
-		newChatroom.setText("New Chatroom");;
+		newChatroom.setText("New Chatroom");
+		;
 		logIn = new JButton("Log in");
 		exit = new JButton("Exit");
-		
+
 		textArea.setEditable(false);
 		textField.setEditable(true);
 
 		button.setText("Send");
-		
+
 		scrollPane1.setBounds(10, 10 + 60, w - 20, h - 20 - 60 - 60);
 		scrollPane2.setBounds(10, h - 60, w - 200, 50);
-		newChatroom.setBounds(10, 10, (w-20-20)/3, 50);
-		logIn.setBounds(1*(w-20)/3 + 15 + 10, 10, (w-20)/3 - 20, 50);
-		exit.setBounds( 2*(w-20)/3 + 10 + 20, 10, (w-20)/3 - 20, 50);
+		newChatroom.setBounds(10, 10, (w - 20 - 20) / 3, 50);
+		logIn.setBounds(1 * (w - 20) / 3 + 15 + 10, 10, (w - 20) / 3 - 20, 50);
+		exit.setBounds(2 * (w - 20) / 3 + 10 + 20, 10, (w - 20) / 3 - 20, 50);
 		button.setBounds(10 + w - 190, h - 60, 170, 50);
 
 		textArea.setFont(new Font("Arial", Font.PLAIN, scrollPane2.getHeight() / 2 + 1));
@@ -216,34 +216,40 @@ public class ChatClient extends JFrame {
 				}
 			}
 		});
-		
+
 		newChatroom.addActionListener(e -> {
 			NewChatroomDialog n = new NewChatroomDialog(this);
 			n.setVisible(true);
 		});
-		
+
 		logIn.addActionListener(e -> {
 			try {
 				logOut();
 				client.close();
 			} catch (NullPointerException | IOException e1) {
-				try {client.close();} catch (Exception e2) {}
+				try {
+					client.close();
+				} catch (Exception e2) {
+				}
 			}
 			login.setVisible(true);
 		});
-		
+
 		exit.addActionListener(e -> {
 			try {
 				logOut();
 				client.close();
 			} catch (NullPointerException | IOException e1) {
-				try {client.close();} catch (Exception e2) {}
+				try {
+					client.close();
+				} catch (Exception e2) {
+				}
 			}
 			setVisible(false);
 			dispose();
 			System.exit(1);
 		});
-		
+
 		button.addActionListener(e -> send());
 
 		contentPane.add(scrollPane1);
@@ -252,29 +258,29 @@ public class ChatClient extends JFrame {
 		contentPane.add(logIn);
 		contentPane.add(exit);
 		contentPane.add(button);
-		
+
 		login = new ChatLogin(this);
 		login.setVisible(true);
 	}
 
 	public void connect() throws UnknownHostException, ConnectException, IOException {
 //		System.out.println("IP: "+ip+", Port: "+port);
-	    client = new Socket(ip, port);
-	    
-	    in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-	    out = new PrintStream(client.getOutputStream());
+		client = new Socket(ip, port);
 
-	    out.println(username);
-	    
-	    try {
-	        CLIENT_ID = Integer.parseInt(in.readLine()); 
-	    } catch (NumberFormatException e1) {
-	        e1.printStackTrace();
-	    } catch (IOException e1) {
-	        e1.printStackTrace();
-	    }
-	    	    	    
-	    executer.submit(new ChatClientThread(this, in, textArea));
+		in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		out = new PrintStream(client.getOutputStream());
+
+		out.println(username);
+
+		try {
+			CLIENT_ID = Integer.parseInt(in.readLine());
+		} catch (NumberFormatException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		executer.submit(new ChatClientThread(this, in, textArea));
 	}
 
 	private void send() {
@@ -292,11 +298,12 @@ public class ChatClient extends JFrame {
 			e.printStackTrace();
 		}
 	}
-	public void logOut () throws NullPointerException, IOException {
+
+	public void logOut() throws NullPointerException, IOException {
 		out.println("/logout");
 		in.readLine();
 	}
-	
+
 	public static void main(String[] args) {
 		ChatClient c = new ChatClient();
 		c.setVisible(true);
@@ -320,7 +327,7 @@ public class ChatClient extends JFrame {
 			setResizable(false);
 			setDefaultCloseOperation(HIDE_ON_CLOSE);
 			setBounds(owner.getX(), owner.getY(), 1080, 720);
-			
+
 			contentPane = getContentPane();
 			contentPane.setLayout(null);
 			contentPane.setBackground(colours[0][1]);
@@ -350,9 +357,9 @@ public class ChatClient extends JFrame {
 				public void actionPerformed(ActionEvent e) {
 					try {
 						String temp = textFieldIP.getText();
-						if (!temp.trim().replaceAll("\n", "").replaceAll(".", "").isEmpty()) {						
+						if (!temp.trim().replaceAll("\n", "").replaceAll(".", "").isEmpty()) {
 							temp = temp.trim().replaceAll("\n", "");
-						} 
+						}
 						ip = temp;
 						if (textFieldPort.getText().trim().replaceAll("\n", "").isEmpty()) {
 							port = DEFAULT_PORT;
@@ -364,9 +371,7 @@ public class ChatClient extends JFrame {
 						connect();
 						setVisible(false);
 					} catch (ConnectException ex) {
-						//new CustomWarningDialog("Values where wrong! No Server found at ip \""+ip+"\" and port \""+port+"\"");
-						//TODO funktioniert net
-						JOptionPane.showMessageDialog(ChatLogin.this, "Values where wrong! No Server found at ip \""+ip+"\" and port \""+port+"\"", "Error", JOptionPane.ERROR_MESSAGE);
+						 new CustomWarningDialog("Values are wrong!");
 					} catch (IOException ex) {
 						new CustomWarningDialog("Couldn't connect to Server!");
 						ex.printStackTrace();
@@ -379,9 +384,9 @@ public class ChatClient extends JFrame {
 			labelUsername.setText("Enter Username: ");
 
 			textFieldIP.setText(DEFAULT_IP);
-			textFieldPort.setText(DEFAULT_PORT+"");
+			textFieldPort.setText(DEFAULT_PORT + "");
 			textFieldUsername.setText(DEFAULT_USERNAME);
-			
+
 			buttonConnect.setText("Connect");
 
 			labelIP.setBounds(0, 150, 314, 100);
@@ -403,14 +408,14 @@ public class ChatClient extends JFrame {
 					try {
 						int por = Integer.parseInt(text);
 						if (por < 1025 || por > 65535) {
-							text = DEFAULT_PORT+"";
+							text = DEFAULT_PORT + "";
 						}
 					} catch (NumberFormatException ex) {
-						text = DEFAULT_PORT+"";
+						text = DEFAULT_PORT + "";
 					}
-	                textFieldPort.setText(text);
+					textFieldPort.setText(text);
 				}
-				
+
 				@Override
 				public void focusGained(FocusEvent e) {
 					textFieldPort.selectAll();
@@ -426,40 +431,39 @@ public class ChatClient extends JFrame {
 					String text = textFieldIP.getText();
 					if (!text.matches("^(\\d{1,3}\\.){3}\\d{1,3}$")) {
 						text = DEFAULT_IP;
-				    }
+					}
 
-				    String[] parts = text.split("\\.");
-				    if (parts.length != 4) {
+					String[] parts = text.split("\\.");
+					if (parts.length != 4) {
 						text = DEFAULT_IP;
-				    }
+					}
 
-				    for (String part : parts) {
-				        try {
-				            int num = Integer.parseInt(part);
-				            if (num < 0 || num > 255) {
+					for (String part : parts) {
+						try {
+							int num = Integer.parseInt(part);
+							if (num < 0 || num > 255) {
 								text = DEFAULT_IP;
 								break;
-				            }
-				        } catch (NumberFormatException ex) {
+							}
+						} catch (NumberFormatException ex) {
 							text = DEFAULT_IP;
 							break;
-				        }
-			        }
+						}
+					}
 					if (!text.equals("localhost")) {
 						text = text.replaceAll("[^0-9.]", "");
-				    }
-							        
-	                textFieldIP.setText(text);
+					}
+
+					textFieldIP.setText(text);
 				}
-				
+
 				@Override
 				public void focusGained(FocusEvent e) {
 					textFieldPort.selectAll();
 				}
 			});
 
-
-			buttonConnect.setBounds(10, 615, getWidth()-20-6, 55);
+			buttonConnect.setBounds(10, 615, getWidth() - 20 - 6, 55);
 
 			contentPane.add(labelIP);
 			contentPane.add(labelPort);
@@ -478,64 +482,64 @@ public class ChatClient extends JFrame {
 			getContentPane().add(titleLable);
 		}
 	}
+
 	private class NewChatroomDialog extends JDialog {
 		private Container contentPane;
 
 		private JLabel nameLabel;
 		private JLabel portLabel;
 		private JLabel passwordLabel;
-		
+
 		private JTextField nameTextfield;
 		private JTextField portTextfield;
 		private JTextField passwordTextfield;
-		
+
 		private JButton create;
 		private JButton close;
 
 		private String name = null;
 		private String password = null;
 
-		
 		public NewChatroomDialog(ChatClient owner) {
 			super(owner, "New Chatroom", true);
 			setResizable(false);
 			setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 			setBounds(owner.getX(), owner.getY(), 1080, 720);
-			
+
 			contentPane = getContentPane();
 			contentPane.setLayout(null);
 			contentPane.setBackground(colours[0][1]);
 
 			nameLabel = new JLabel();
 			nameLabel.setForeground(colours[0][3]);
-			
+
 			portLabel = new JLabel();
 			portLabel.setForeground(colours[0][3]);
-			
+
 			passwordLabel = new JLabel();
 			passwordLabel.setForeground(colours[0][3]);
-			
+
 			nameTextfield = new JTextField();
 			nameTextfield.setBackground(colours[0][2]);
 			nameTextfield.setForeground(colours[0][3]);
 			nameTextfield.setBorder(null);
-			
+
 			portTextfield = new JTextField();
 			portTextfield.setBackground(colours[0][2]);
 			portTextfield.setForeground(colours[0][3]);
 			portTextfield.setBorder(null);
-			
+
 			passwordTextfield = new JTextField();
 			passwordTextfield.setBackground(colours[0][2]);
 			passwordTextfield.setForeground(colours[0][3]);
-			passwordTextfield.setBorder(null);			
-			
+			passwordTextfield.setBorder(null);
+
 			create = new JButton();
 			create.setBackground(colours[0][4]);
 			create.setForeground(colours[0][2]);
 			create.setFocusPainted(false);
 			create.setBorderPainted(false);
-			
+
 			close = new JButton();
 			close.setBackground(colours[0][4]);
 			close.setForeground(colours[0][2]);
@@ -549,44 +553,43 @@ public class ChatClient extends JFrame {
 			nameTextfield.setText("");
 			portTextfield.setText("");
 			passwordTextfield.setText("");
-			
+
 			create.setText("Create");
 			close.setText("Close");
 
-			nameLabel.setBounds(10, 10, 2*getWidth()/5-6-20, 50);
-			nameLabel.setFont(new Font("Arial", Font.PLAIN, nameLabel.getHeight()/2+1));
-			
-			portLabel.setBounds(10, 70, 2*getWidth()/5-6-20, 50);
-			portLabel.setFont(new Font("Arial", Font.PLAIN, portLabel.getHeight()/2+1));
-			
-			passwordLabel.setBounds(10, 130, 2*getWidth()/5-6-20, 50);
-			passwordLabel.setFont(new Font("Arial", Font.PLAIN, portLabel.getHeight()/2+1));
-			
-			nameTextfield.setBounds(10 + 2*getWidth()/5-6-10, 10, 3*getWidth()/5-6-20, 50);
-			nameTextfield.setFont(new Font("Arial", Font.PLAIN, nameTextfield.getHeight()/2+1));
-			nameTextfield.setBorder(null);
-			
-			portTextfield.setBounds(10 + 2*getWidth()/5-6-10, 70, 3*getWidth()/5-6-20, 50);
-			portTextfield.setFont(new Font("Arial", Font.PLAIN, portTextfield.getHeight()/2+1));
-			portTextfield.setBorder(null);
-			
-			passwordTextfield.setBounds(10 + 2*getWidth()/5-6-10, 130, 3*getWidth()/5-6-20, 50);
-			passwordTextfield.setFont(new Font("Arial", Font.PLAIN, portTextfield.getHeight()/2+1));
-			passwordTextfield.setBorder(null);
-			
+			nameLabel.setBounds(10, 10, 2 * getWidth() / 5 - 6 - 20, 50);
+			nameLabel.setFont(new Font("Arial", Font.PLAIN, nameLabel.getHeight() / 2 + 1));
 
-			create.setBounds(10, 615, ((getWidth()-6)/2)-10, 55);
-			close.setBounds(((getWidth()-6)/2)+10, 615, ((getWidth()-6)/2)-10, 55);
-			create.setFont(new Font("Arial", Font.PLAIN, create.getHeight()/2+1));
-			close.setFont(new Font("Arial", Font.PLAIN, close.getHeight()/2+1));
+			portLabel.setBounds(10, 70, 2 * getWidth() / 5 - 6 - 20, 50);
+			portLabel.setFont(new Font("Arial", Font.PLAIN, portLabel.getHeight() / 2 + 1));
+
+			passwordLabel.setBounds(10, 130, 2 * getWidth() / 5 - 6 - 20, 50);
+			passwordLabel.setFont(new Font("Arial", Font.PLAIN, portLabel.getHeight() / 2 + 1));
+
+			nameTextfield.setBounds(10 + 2 * getWidth() / 5 - 6 - 10, 10, 3 * getWidth() / 5 - 6 - 20, 50);
+			nameTextfield.setFont(new Font("Arial", Font.PLAIN, nameTextfield.getHeight() / 2 + 1));
+			nameTextfield.setBorder(null);
+
+			portTextfield.setBounds(10 + 2 * getWidth() / 5 - 6 - 10, 70, 3 * getWidth() / 5 - 6 - 20, 50);
+			portTextfield.setFont(new Font("Arial", Font.PLAIN, portTextfield.getHeight() / 2 + 1));
+			portTextfield.setBorder(null);
+
+			passwordTextfield.setBounds(10 + 2 * getWidth() / 5 - 6 - 10, 130, 3 * getWidth() / 5 - 6 - 20, 50);
+			passwordTextfield.setFont(new Font("Arial", Font.PLAIN, portTextfield.getHeight() / 2 + 1));
+			passwordTextfield.setBorder(null);
+
+			create.setBounds(10, 615, ((getWidth() - 6) / 2) - 10, 55);
+			close.setBounds(((getWidth() - 6) / 2) + 10, 615, ((getWidth() - 6) / 2) - 10, 55);
+			create.setFont(new Font("Arial", Font.PLAIN, create.getHeight() / 2 + 1));
+			close.setFont(new Font("Arial", Font.PLAIN, close.getHeight() / 2 + 1));
 
 			create.addActionListener(e -> create());
-			
+
 			close.addActionListener(e -> {
 				setVisible(false);
 				dispose();
 			});
-			
+
 			contentPane.add(nameLabel);
 			contentPane.add(portLabel);
 			contentPane.add(passwordLabel);
@@ -598,7 +601,8 @@ public class ChatClient extends JFrame {
 			contentPane.add(create);
 			contentPane.add(close);
 		}
-		private void create () {			
+
+		private void create() {
 			name = nameTextfield.getText().trim().replaceAll("\n", "");
 			if (portTextfield.getText().trim().replaceAll("\n", "").isEmpty()) {
 				port = DEFAULT_PORT;
@@ -606,26 +610,35 @@ public class ChatClient extends JFrame {
 				port = Integer.valueOf(portTextfield.getText());
 			}
 			password = passwordTextfield.getText().trim().replaceAll("\n", "");
-			
+
 			try {
-				out.println("/Chatroom("+name+", "+password+", "+port+")");
+				out.println("/Chatroom(" + name + ", " + password + ", " + port + ")");
 			} catch (Exception e1) {
-				try {client.close();} catch (Exception e2) {}
+				try {
+					client.close();
+				} catch (Exception e2) {
+				}
 			}
-			
+
 			try {
 				logOut();
 				client.close();
 			} catch (NullPointerException | IOException e1) {
-				try {client.close();} catch (Exception e2) {}
+				try {
+					client.close();
+				} catch (Exception e2) {
+				}
 			}
-			
+
 			try {
 				connect();
 			} catch (NullPointerException | IOException e1) {
-				try {client.close();} catch (Exception e2) {}
+				try {
+					client.close();
+				} catch (Exception e2) {
+				}
 			}
-			
+
 			setVisible(false);
 			dispose();
 		}
